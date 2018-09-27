614e461b4e08d3c9b9d95037406689b7bb2a77bd
Alex Larson <alex.m.larson@gmail.com>
2018-09-27 08:21:16 -0700

    Add project files.

---
+++ MarketOMatic/ImportLogic/YouTubeLogic.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketOMatic.Logic
+{
+    class YouTubeLogic
+    {
+    }
+}
