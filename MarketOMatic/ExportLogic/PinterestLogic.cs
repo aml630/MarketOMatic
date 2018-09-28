9d62af9a31e4ba007e5fd6a43f9b2df4106a9fb1
Alex Larson <alex.m.larson@gmail.com>
2018-09-27 18:56:53 -0700

    added pinterest and twitter share logic

--- MarketOMatic/ExportLogic/PinterestLogic.cs
+++ MarketOMatic/ExportLogic/PinterestLogic.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RestSharp;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,30 @@ namespace MarketOMatic.ExportLogic
 {
     class PinterestLogic
     {
+        public string AutoPin()
+        {
+            var accessToken = "AYU2Mge9YgxVP9EdfMz-fQUcgV1bFRCagVrgOUFErq7vBsA-CgAAAAA";
+            var boardNum = "850687885802909063";
+
+            var url = "Url Link";
+            var imageUrl = "Image URl";
+            var notes = "Title of image";
+
+
+            IRestResponse response = Pin(accessToken, boardNum, url, imageUrl, notes);
+
+            return response.Content;
+        }
+
+        private static IRestResponse Pin(string accessToken, string boardNum, string url, string imageUrl, string notes)
+        {
+            var client = new RestClient("https://api.pinterest.com/v1/pins/?access_token=" + accessToken + "&fields=id%2Clink%2Cnote%2Curl");
+            var request = new RestRequest(Method.POST);
+            request.AddHeader("content-type", "multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW");
+            request.AddParameter("multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW", "------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"board\"\r\n\r\n" + boardNum + "\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"link\"\r\n\r\n" + url + "\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"image_url\"\r\n\r\n" + imageUrl + "\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"note\"\r\n\r\n" + notes + "\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW--", ParameterType.RequestBody);
+
+            IRestResponse response = client.Execute(request);
+            return response;
+        }
     }
 }
--- MarketOMatic/ExportLogic/TwitterLogic.cs
+++ MarketOMatic/ExportLogic/TwitterLogic.cs
@@ -3,10 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
 
 namespace MarketOMatic.ExportLogic
 {
     class TwitterLogic
     {
+        public string Tweet(string message, string imageSource = null)
+        {
+
+            string ConsumerKey = "j3HsSc1ejljPQlrKite0yiGAe",
+            ConsumerKeySecret = "2H0CGLfnO3IhSF7yEmt9gzfKDEA5W6eDBVGOHk6hVrToorsHRF",
+            AccessToken = "919319179529818113-UQkL7yk4SoSk9RccTfogPhfOOX5biJT",
+            AccessTokenSecret = "KqZleU9ADCXZe6KF0IjvJWCTWpeyY4OF7BZZbYVd62tqa";
+
+            var twitter = new Twitter(ConsumerKey, ConsumerKeySecret, AccessToken, AccessTokenSecret);
+
+            if (!string.IsNullOrEmpty(imageSource))
+            {
+                string imagePath = HttpRuntime.AppDomainAppPath + imageSource;
+
+                return twitter.PublishToTwitter(message, imagePath);
+            }
+            else
+            {
+                var rez = Task.Run(async () =>
+                {
+                    return await twitter.TweetText(message, string.Empty);
+                });
+            }
+
+            return "error";
+        }
     }
 }
