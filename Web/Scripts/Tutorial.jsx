c0e35347853564193c5e71a13705a4b3189b0945
Alex Larson <alex.m.larson@gmail.com>
2018-10-01 07:38:56 -0700

    added react

---
+++ Web/Scripts/Tutorial.jsx
@@ -0,0 +1,34 @@
+﻿class CommentBox extends React.Component {
+    render() {
+        return (
+            <div className="commentBox">
+                Hello, world! I am a CommentBoddx.
+            </div>
+        );
+    }
+}
+
+class CommentForm extends react.Component {
+    redner() {
+        return (
+            <div className="commentList">
+                Hello I'm commentList.
+                </div>
+        );
+    }
+}
+
+//class CommentForm extends react.Component {
+//    redner() {
+//        return (
+//            <div className="commentForm">
+//                Hello I'm commentForm.
+//                </div>
+//        );
+//    }
+//}
+
+ReactDOM.render(
+    <CommentBox />,
+    document.getElementById('content')
+);
